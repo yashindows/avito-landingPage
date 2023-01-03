e5c8202ef8c98d127bc3d9ea14ab7741ded40389
yashindows <105119402+yashindows@users.noreply.github.com>
2023-01-03 18:19:05 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// const animItems = document.querySelectorAll('._anim-item');
+// if (animItems.length > 0){
+//     window.addEventListener('scroll', animOnScroll);
+//     function animOnScroll(){
+//         for (let index=0; index < animItems; index++){
+//             const animItem = animItems[index];
+//             const animItemHeight = animItem.offsetHeight;
+//             const animItemOffset = offset(animItem).top;
+//             const animStart = 4;
+//             let animItemPoint = window.innerHeight - animItemHeight / animStart;
+//             if(animItemHeight > window.innerHeight){
+//                 animItemPoint = window.innerHeight - window.innerHeight / animStart;
+//             }
+//             if((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)){
+//                 animItem.classList.add('_active');
+//             }
+//             else{
+//                 animItem.classList.remove('_active');
+//             }
+//         }
+//     }
+//     function offset(el){
+//         const rect = el.getBoundingClientRect(),
+//             scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+//             scrollTop = window.scrollY || document.documentElement.scrollTop;
+//             return{top: rect.top + scrollTop, left: rect.left + scrollLeft}
+//     }
+//     animOnScroll();
+// }
+let mediaQuery = window.matchMedia('(max-width: 860px)');
+let removeAnim = document.querySelector('.promo-form-wrapper');
+let tipNoAnim = document.querySelector('.promo-tip')
+if (mediaQuery.matches){
+  removeAnim.classList.add('_visible');
+  console.log('ti pidoor');
+  tipNoAnim.classList.add('_visible')
+}
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+for (let elm of elements) {
+  observer.observe(elm);
+}
